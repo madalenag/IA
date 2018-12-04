a7e87205d93a7363e2e5fe7bd1df778fa7f117f5
taissahr <koalatai@LAPTOP-FMO1E51U.localdomain>
2018-12-04 22:08:58 +0000

    OK

--- proj2/RL.py
+++ proj2/RL.py
@@ -16,7 +16,7 @@ import random
 
 from tempfile import TemporaryFile
 outfile = TemporaryFile()
-	
+
 class finiteMDP:
 
     def __init__(self, nS, nA, gamma, P=[], R=[], absorv=[]):
@@ -28,8 +28,8 @@ class finiteMDP:
         self.R = R
         self.absorv = absorv
         # completar se necessario
-        
-            
+
+
     def runPolicy(self, n, x0,  poltype = 'greedy', polpar=[]):
         #nao alterar
         traj = np.zeros((n,4))
@@ -44,7 +44,7 @@ class finiteMDP:
             if self.absorv[x]:
                 y = x0
             x = y
-        
+
         return J,traj
 
 
@@ -52,63 +52,60 @@ class finiteMDP:
         #nao alterar
         nQ = np.zeros((self.nS,self.nA))
         while True:
-            self.V = np.max(self.Q,axis=1) 
+            self.V = np.max(self.Q,axis=1)
             for a in range(0,self.nA):
                 nQ[:,a] = self.R[:,a] + self.gamma * np.dot(self.P[:,a,:],self.V)
             err = np.linalg.norm(self.Q-nQ)
             self.Q = np.copy(nQ)
             if err<1e-7:
                 break
-            
+
         #update policy
-        self.V = np.max(self.Q,axis=1) 
+        self.V = np.max(self.Q,axis=1)
         #correct for 2 equal actions
         self.Pol = np.argmax(self.Q, axis=1)
-                    
+
         return self.Q,  self.Q2pol(self.Q)
 
-            
+
     def traces2Q(self, trace):
         self.Q = np.zeros((self.nS,self.nA))
         newQ = np.copy(self.Q)
         i = 0
         dif = 1
-        alpha = 0.1
+        alpha = 0.5
 
-        while dif > 0.01:
-            for line in trace:
-                iS = int(line[0])
-                a = int(line[1])
-                nS = int(line[2])
-                r = int(line[3])
+        while dif > 0.00000000000001:
+            for t in trace:
+                iS = int(t[0])
+                a = int(t[1])
+                nS = int(t[2])
+                r = int(t[3])
 
-                newQ[iS,a] = self.Q[iS,a] + alpha * (r + self.gamma * max(self.Q[nS,:]) - self.Q[iS,a])
-                
-                dif = np.linalg.norm(self.Q - newQ)
-                self.Q = np.copy(newQ)
+                self.Q[iS,a] = self.Q[iS,a] + alpha * (r + self.gamma * max(self.Q[nS,:]) - self.Q[iS,a])
+
+                dif = np.linalg.norm(newQ - self.Q)
+                newQ = np.copy(self.Q)
 
         return self.Q
-    
-    
+
+
     def policy(self, x, poltype = 'exploration', par = []):
         # implementar esta funcao
-        
+
         if poltype == 'exploitation':
-            a = self.Q[x].argmax()
-            print(self.Q[x,0])
-            print(self.Q[x,1])
+            a = par[x].argmax()
+            print(par[x,0])
+            print(par[x,1])
             print(a)
 
         elif poltype == 'exploration':
-            a = random.randint(0, len(self.Q[x])-1)
+            a = random.randint(0, self.nA-1)
 
-                
+
         return a
-    
-    
+
+
     def Q2pol(self, Q, eta=5):
         # implementar esta funcao
         return np.exp(eta*Q)/np.dot(np.exp(eta*Q),np.array([[1,1],[1,1]]))
-
-
-            
